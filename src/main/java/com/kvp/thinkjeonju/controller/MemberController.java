efc12621dafbaecc0c2787f97516797e7ed9f8e4
MINDOCK <parksy8189@gmail.com>
2018-10-27 20:19:17 +0900

    feat: member 프론트 구현 및 기본 화면 구성
    1. modal을 통한 로그인, 회원가입
    2. 로그인, 회원가입, 로그아웃 ajax 통신
    3. 홈페이지 상단, 하단 구성

--- src/main/java/com/kvp/thinkjeonju/controller/MemberController.java
+++
@@ -1,9 +0,0 @@
-package com.kvp.thinkjeonju.controller;
-
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
-
-@Controller
-@RequestMapping("/members")
-public class MemberController {
-}
