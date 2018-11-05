97d25930177653902c001c23123de337e326d577
MINDOCK <parksy8189@gmail.com>
2018-11-05 14:02:02 +0900

    refactor: input 자동완성 금지 및 좋아요 기능 개선
    1. autocomplete = 'off' : 자동완성 금지
    2. unAuthenticatedException 발생시, HttpStatus.FORBIDDEN 반환

--- src/main/java/com/kvp/thinkjeonju/advice/common/CommonExceptionHandler.java
+++ src/main/java/com/kvp/thinkjeonju/advice/common/CommonExceptionHandler.java
@@ -2,14 +2,17 @@ package com.kvp.thinkjeonju.advice.common;
 
 import com.kvp.thinkjeonju.exception.common.UnAuthenticatedException;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
 
 @Slf4j
 @ControllerAdvice
 public class CommonExceptionHandler {
 
     @ExceptionHandler(UnAuthenticatedException.class)
+    @ResponseStatus(HttpStatus.FORBIDDEN)
     public String loginUserNotFound(UnAuthenticatedException exception) {
         log.error("[UnAuthenticatedException] {}", exception.getMessage());
         return "/error";
