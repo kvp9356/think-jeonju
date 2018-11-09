e5ef53013acc56b52bd0aaf07034acad7023e49d
Van Sun Ho <vsh123@naver.com>
2018-11-10 02:43:28 +0900

    1. ApiScheduleSearchController 삭제
    2. ApiSpotController 수정
     - 기존의 ScheduleSearchController에 있던 메소드가 해당 컨트롤러에 더
     적합하다 판단되어  이전
    
    3. ScheduleController 수정
     - Mapping URL 수정
    
    4. 예산 기능 수정
     - 기존 한 장소에서 하나의 예산만 입력가능한 방식에서 현재는 한 장소당
     최대 5개의 예산을 저장할 수 있으며, 각 예산별 이름 명명이 가능
     - 이름의 최대 제한 글자 : 10자
     - 최대 적을수 있는 가격 : 9,999,999원
    
     5. 스케줄 작성 버그 수정
      - 2일차에서 1일차로 축소시 기존 일정이 사라지지 않는점 주성

--- src/main/java/com/kvp/thinkjeonju/controller/ScheduleController.java
+++ src/main/java/com/kvp/thinkjeonju/controller/ScheduleController.java
@@ -12,7 +12,7 @@ import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
 @Controller
-@RequestMapping("/schedule")
+@RequestMapping("/schedules")
 public class ScheduleController {
 
     @GetMapping("")
--- src/main/resources/static/js/schedule.js
+++ src/main/resources/static/js/schedule.js
@@ -1,21 +1,23 @@
 var nowDay;
 var $tabs = $(".left-frame").tabs();
 
+
+
 var drawSchedule = function(){
     var a = $($("#tab-frame>.ui-tabs-active>a").attr("href")).find(".spotimg");
     let html="";
     for(count=0;count<a.length;count++){
         var name = $(a[count]).parent().find(".spotId").attr("class").split(' ')[0]
         var x = a[count].getAttribute("src");
-        const constHTML ="<div class=\"draw\">\n" +
-            "                        <img src=\""+ x +"\"  class=\"spotimg\">\n" +
-            "                        <div class=\""+ name +" spotId\"></div>\n" +
+        const constHTML ="<div class='draw'>\n" +
+            "                        <img src='"+ x +"'  class='spotimg'>\n" +
+            "                        <div class='"+ name +" spotId'></div>\n" +
             "                    </div>\n" +
-            "<img src=\"/image/right-arrow.png\" class=\"arrowimg\">\n";
+            "<img src='/image/right-arrow.png' class='arrowimg'>\n";
         html+=constHTML;
     }
     if(html!="")
-        html=html+"<img src=\"/image/finish-flag.png\" class=\"arrowimg\">\n";
+        html=html+"<img src='/image/finish-flag.png' class='finish_flag'>\n";
     var day= $("#tab-frame>.ui-tabs-active>a").attr("href").substring(12);
     $(".drawSchedule").find(".day"+day+">.drawDay").html(html);
 }
@@ -26,15 +28,15 @@ var drawdropSchedule = function(move){
     for(count=0;count<a.length;count++){
         var name = $(a[count]).parent().find(".spotId").attr("class").split(' ')[0]
         var x = a[count].getAttribute("src");
-        const constHTML ="<div class=\"draw\">\n" +
-            "                        <img src=\""+ x +"\"  class=\"spotimg\">\n" +
-            "                        <div class=\""+ name +"\"></div>\n" +
+        const constHTML ="<div class='draw'>\n" +
+            "                        <img src='"+ x +"'  class='spotimg'>\n" +
+            "                        <div class='"+ name +" spotId'></div>\n" +
             "                    </div>\n" +
-            "<img src=\"/image/right-arrow.png\" class=\"arrowimg\">\n";
+            "<img src='/image/right-arrow.png' class='arrowimg'>\n";
         html+=constHTML;
     }
     if(html!="")
-        html=html+"<img src=\"/image/finish-flag.png\" class=\"arrowimg\">\n";
+        html=html+"<img src='/image/finish-flag.png' class='finish_flag'>\n";
     var day= move.attr("href").substring(12)
     $(".drawSchedule").find(".day"+day+">.drawDay").html(html);
 
@@ -44,7 +46,7 @@ $("#spotSearch").on({
     click: function() {
         var dataValue = $("#searchKeyword").val();
         $.ajax({
-            url: "/schedule/search",
+            url: "/api/spots/search",
             method: "POST",
             data: {
                 dataValue : dataValue
@@ -56,13 +58,13 @@ $("#spotSearch").on({
                 }
                 $("#searchResult").html(html);
                 spots.forEach(spot => {
-                    const spotHTML = "<div class=\"details-Spot\">\n" +
-                        "                <div class=\"" + spot.id + " spotId\"></div>" +
-                        "                <img src=\"" + spot.imgUrl[0] + "\" class=\"spotimg\">\n" +
-                        "                <dl class=\"spotInfo\">\n" +
-                        "                    <dd class=\"category\">" + spot.category + "</dd>\n" +
-                        "                    <dt class=\"title\">" + spot.name + "</dt>\n" +
-                        "                    <dd class=\"like\"><img src=\"/image/fullStar.png\" alt=\"\"/> <spn class=\"like-cnt\">" + spot.likeCnt + "</spn></dd>\n" +
+                    const spotHTML = "<div class='details-Spot'>\n" +
+                        "                <div class='" + spot.id + " spotId'></div>" +
+                        "                <img src='" + spot.imgUrl[0] + "' class='spotimg'>\n" +
+                        "                <dl class='spotInfo'>\n" +
+                        "                    <dd class='category'>" + spot.category + "</dd>\n" +
+                        "                    <dt class='title'>" + spot.name + "</dt>\n" +
+                        "                    <dd class='like'><img src='/image/fullStar.png' alt=''/> <spn class='like-cnt'>" + spot.likeCnt + "</spn></dd>\n" +
                         "                </dl>" +
                         "            </div>";
                 html += spotHTML;
@@ -74,7 +76,10 @@ $("#spotSearch").on({
                     connectWith: ".details-Schedule",
                     placeholder: "ui-state-highlight",
                     start: function(e,ui){
+                        ui.item.height(300);
                         ui.placeholder.height(ui.item.height());
+                        $(".details_receipt").css("display","none");
+                        $(".receiptimg").css("background-color","white");
                     },
                     change: function(e, ui) {
                         $(ui.placeholder).parent().find(".default-text").hide();
@@ -84,7 +89,8 @@ $("#spotSearch").on({
                             $("#searchResult").append($(ui.item).clone());
                         }
                         $("#searchResult>.details-Spot>.x_button").remove();
-                        $("#searchResult>.details-Spot>.budget").remove();
+                        $("#searchResult>.details-Spot>dl>.receiptimg").remove();
+                        $("#searchResult>.details-Spot>dl>.details_receipt").remove();
                         if($($("#tab-frame>.ui-tabs-active>a").attr("href")).find(".details-Spot").length==1){
                             $($("#tab-frame>.ui-tabs-active>a").attr("href")).find(".default-text").show();
                         }
@@ -130,26 +136,26 @@ $(".btn-primary").on({
             var tab_form;
             var draw_form;
             for(count=1;count<=day;count++) {
-                if (($("#day" + count)).length == 0) {            //해당 id가 미존재한다면
-                    day_Form = "<div id=\"scheduleday" + count + "\" class=\"day-Form day\">\n" +
-                        "                  <div id=\"detailday"+count+"\" class=\"details-Schedule\">\n" +
-                        "                 <div class=\"details-Spot default-text\">원하는 장소를 드래그해서 가져오세요!</div>\n" +
+                if (($("#scheduleday" + count)).length == 0) {            //해당 id가 미존재한다면
+                    day_Form = "<div id='scheduleday" + count + "' class='day-Form day'>\n" +
+                        "                  <div id='detailday"+count+"' class='details-Schedule'>\n" +
+                        "                 <div class='details-Spot default-text'>원하는 장소를 드래그해서 가져오세요!</div>\n" +
                         "                 </div>\n" +
                         "            </div>";
                     $("#day-frame").append(day_Form);
-                    tab_form = "<li class=\"tab\" id=\"tab"+count+"\"><a href=\"#scheduleday" + count + "\">"+ count +"일차</a></li>";
+                    tab_form = "<li class='tab' id='tab"+count+"'><a href='#scheduleday" + count + "'>"+ count +"일차</a></li>";
                     $("#tab-frame").append(tab_form);
-                    draw_form="<div id=\"drawDay"+count+"\" class=\"draw-form day"+count+"\">\n" +
-                        "                <div class=\"nameDay\">"+count+" 일차</div>\n" +
-                        "                <div class=\"drawDay\">\n" +
+                    draw_form="<div id='drawDay"+count+"' class='draw-form day"+count+"'>\n" +
+                        "                <div class='nameDay'>"+count+" 일차</div>\n" +
+                        "                <div class='drawDay'>\n" +
                         "                </div>\n" +
                         "            </div>";
                     $(".drawSchedule").append(draw_form);
                 }
             }
             for(count;count<=nowDay;count++){
-                if (($("#day" + count)).length != 0) {            //해당 id가 존재한 다면
-                    $("#day"+count).remove();
+                if (($("#scheduleday" + count)).length != 0) {            //해당 id가 존재한 다면
+                    $("#scheduleday"+count).remove();
                     $("#tab"+count).remove()
                     $("#drawDay"+count).remove();
                 }
@@ -173,9 +179,12 @@ $(".btn-primary").on({
         $(".details-Schedule").sortable({
             cursorAt : { left : 200, top : 150 },
             placeholder: "ui-state-highlight",
-            cancel: ".default-text,input",
+            cancel: ".default-text,.details_receipt",
             start: function(e,ui){
+                ui.item.height(300);
                 ui.placeholder.height(ui.item.height());
+                $(".details_receipt").css("display","none");
+                $(".receiptimg").css("background-color","white");
             },
             beforeStop: function(e, ui) {
                 drawSchedule();
@@ -186,12 +195,16 @@ $(".btn-primary").on({
                     ui.sender.sortable('cancel');
                 }
                 else if($(ui.item).parent().attr("id")!="searchResult"&& $(ui.item).find(".x_button").length==0) {
-                    var imagetag = "<img src=\"/image/x_button.png\" class=\"x_button\">";
-                    var budgettag = "<div class=\"budget\">\n예산(단위:원):" +
-                        "<input type=\"number\" class=\"details-budget\" min=\"0\" step=\"500\" numberOnly>\n" +
-                        "</div>"
+                    var imagetag = "<img src='/image/x_button.png' class='x_button'>";
+                    var receipttag =
+                            "<img src='/image/receipt.png' class='receiptimg'>" +
+                        "<div class='details_receipt'>" +
+                            "<img src='/image/plus_button.png' class='plus_button'>" +
+                        "   <input type='text' class='default_receipt' value='이름(ex.식대) : 금액(단위:원)' readonly>" +
+                        "</div>";
+
                     $(ui.item).prepend(imagetag);
-                    $(ui.item).find(".spotInfo").append(budgettag);
+                    $(ui.item).find(".spotInfo").append(receipttag);
                     drawSchedule();
                 }
             }
@@ -219,8 +232,80 @@ $(".btn-primary").on({
                         }
                     }
                 }
+                $(".details_receipt").css("display","none");
+                $(".receiptimg").css("background-color","white");
             }
         });
+    }
+});
+
+$(document).on("click",".receiptimg",function(){
+    if($(this).parent().find(".details_receipt").css("display")=="block") {
+        $(this).parent().find(".details_receipt").css("display", "none");
+        $(this).css("background-color","white");
+    }
+    else {
+        $(this).parent().find(".details_receipt").css("display", "block");
+        $(this).css("background-color","lightgray");
+    }
+});
+
+$(document).on("click",".finish_flag",function(){
+    var summary = $(this).parent().parent();
+    var day = summary.attr("id").substring(7);
+    var sumspot = summary.find(".draw").length;
+    var summoney = $("#detailday"+day).find(".budget_money");
+    var totalmoney=0;
+    for(var i=0; i< summoney.length;i++){
+        totalmoney+=Number(summoney[i].value);
+    }
+
+    totalmoney=totalmoney.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+
+    var header = day+"일차 통계";
+    var body = "총 여행 장소 : " + sumspot +"개 장소<br>" +
+        "총 예상 경비 : " + totalmoney+"원";
+    $("#spotName").html(header);
+    $("#spotModal-body").html(body);
+    $("#spotModal").modal("show");
+});
+
+
+/*$("#schedule-frame").scroll(function () {
+    $(".details_receipt").css("display","none");
+    $(".receiptimg").css("background-color","white");
+});*/
+
+$(document).on("click",".plus_button",function(){
+    let a =$(this).parent().find(".budget").length;
+    if(a>4){
+        alert("한 장소당 추가가능한 예산은 최대 5개 입니다.");
+    }
+    else {
+        const html = "<div class='budget'>" +
+            "<input type='text' class='budget_name'>:" +
+            "<input type='number' class='budget_money' min='0' step='500'>" +
+            "<img src='/image/minus_button.png' class='minus_button'>" +
+            "</div>";
+        $(this).parent().append(html);
+    }
+});
+
+$(document).on("click",".minus_button",function(){
+    $(this).parent().remove();
+});
+
+$(document).on("keyup",".budget_name",function(e){
+    if($(this).val().length>10){
+        alert("제목은 10자 미만으로 적어주세요");
+        $(this).val($(this).val().substring(0,10));
+    }
+});
+
+$(document).on("keyup",".budget_money",function(e){
+    if($(this).val().length>7){
+        alert("한 주제에 대한 예산은 999만원을 넘길 수 없습니다.");
+        $(this).val($(this).val().substring(0,7));
     }
 });
 
@@ -240,18 +325,33 @@ $(document).on("click",".draw",function(){
     var head = '';
     head += spot.find(".title").text();
     var html='';
-    html +="<img src=\""+ $(this).find(".spotimg").attr("src")+"\"  class=\"spotimg\">\n" +
-        "                    <dd class=\"like\">\n" +
-        "                        <img src=\"/image/fullStar.png\">\n" +
-        "                        <spn class=\"like-cnt\">"+ spot.find(".like-cnt").text()+"</spn>\n" +
+    html +="<img src='"+ $(this).find(".spotimg").attr("src")+"'  class='spotimg'>\n" +
+        "                    <dd class='like'>\n" +
+        "                        <img src='/image/fullStar.png'>\n" +
+        "                        <spn class='like-cnt'>"+ spot.find(".like-cnt").text()+"</spn>\n" +
         "                    </dd>\n" +
-        "                    <div class=\"budget\">\n" +
-        "                        예산(단위:원):<input type=\"text\" class=\"details-budget\" readonly value=\""+ spot.find(".details-budget").val() +"\">\n" +
-        "                    </div>";
+        "    <img src='/image/receipt.png' class='receiptimg'>" +
+        "<div class='details_receipt'>" +
+        "   <input type='text' class='default_receipt' value='이름(ex.식대) : 금액(단위:원)' readonly>";
+    let budget_name = spot.find(".budget_name");
+    let budget_money = spot.find(".budget_money");
+    for(var i =0; i< budget_name.length;i++)
+    {
+        if(budget_name[0].value.replace(/ /gi, "").length!=0&&budget_money[i].value.length!=0) {
+            var receipt = "<div class='budget'>" +
+                "<input type='text' class='budget_name' value='" + budget_name[i].value + "' readonly>:" +
+                "<input type='number' class='budget_money' value='" + budget_money[i].value + "' readonly> " +
+                "</div>"
+            html += receipt;
+        }
+    }
+    html += "</div>";
     $("#spotName").html(head);
     $("#spotModal-body").html(html);
     $("#spotModal").modal("show");
 });
+
+
 
 
 
@@ -272,13 +372,16 @@ $(document).ready(function(){
 
 });
 
+$("#spotModal-footer>.btn-secondary").on("click",function(){
+    $("#spotModal").modal("hide");
+});
+
 $('#fromDate').datepicker({
     dateFormat: 'yy-mm-dd' //Input Display Format 변경
     ,showOtherMonths: true //빈 공간에 현재월의 앞뒤월의 날짜를 표시
     ,showMonthAfterYear:true //년도 먼저 나오고, 뒤에 월 표시
     ,changeYear: true //콤보박스에서 년 선택 가능
     ,changeMonth: true //콤보박스에서 월 선택 가능
-//    ,showOn: "both" //button:버튼을 표시하고,버튼을 눌러야만 달력 표시 ^ both:버튼을 표시하고,버튼을 누르거나 input을 클릭하면 달력 표시
     ,buttonImage: "http://jqueryui.com/resources/demos/datepicker/images/calendar.gif" //버튼 이미지 경로
     ,buttonImageOnly: true //기본 버튼의 회색 부분을 없애고, 이미지만 보이게 함
     ,buttonText: "선택" //버튼에 마우스 갖다 댔을 때 표시되는 텍스트
@@ -295,7 +398,6 @@ $('#toDate').datepicker({
     ,showMonthAfterYear:true //년도 먼저 나오고, 뒤에 월 표시
     ,changeYear: true //콤보박스에서 년 선택 가능
     ,changeMonth: true //콤보박스에서 월 선택 가능
-//    ,showOn: "both" //button:버튼을 표시하고,버튼을 눌러야만 달력 표시 ^ both:버튼을 표시하고,버튼을 누르거나 input을 클릭하면 달력 표시
     ,buttonImage: "http://jqueryui.com/resources/demos/datepicker/images/calendar.gif" //버튼 이미지 경로
     ,buttonImageOnly: true //기본 버튼의 회색 부분을 없애고, 이미지만 보이게 함
     ,buttonText: "선택" //버튼에 마우스 갖다 댔을 때 표시되는 텍스트
